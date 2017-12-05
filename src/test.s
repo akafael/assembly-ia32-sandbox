437c181ae29ce782eb48d5cc6e9fae36eaa5abaa
Rafael <raffaellimma@gmail.com>
2017-12-05 12:07:45 -0200

    Files name ordered

--- src/test.s
+++ src/test.s
@@ -1,39 +1,23 @@
 ; =========================================================
-; Loop - Print the Alphabet
+; Hello Word
 ;
-; @author Rafael Lima
 ; =========================================================
-; Constants -----------------------------------------------
-SYS_EXIT: equ 1
+
 ; Code Segment --------------------------------------------
-section .text
-    global _start
+segment .text           ; code section
+    global _start       ; make label available to linker
+_start:                 ; standard  gcc  entry point
+        mov edx,len     ; arg3, length of string to print
+        mov ecx,msg     ; arg2, pointer to string
+        mov ebx,1       ; arg1, where to write, screen
+        mov eax,4       ; write sysout command to int 80 hex
+        int 0x80        ; interrupt 80 hex, call kernel
 
-_start:
-    mov ecx, 26         ; Set limit as the Alphabet size
-    mov eax, 'A'        ; Pick the first letter
+        mov ebx,0       ; exit code, 0=normal
+        mov eax,1       ; exit command to kernel
+        int 0x80        ; interrupt 80 hex, call kernel
 
-l1:
-    mov [num], AL       ; Overwrite the first byte
-    push ecx            ; Recover ECX value from stack
-
-    mov edx,2           ; Length of string to print ('char'+0xA)
-    mov ecx,num         ; Pointer to string
-    mov ebx,1           ; where to write (stdio)
-    mov eax,4           ; chose sys_out command to int 80 hex
-    int 0x80            ; interrupt 80 hex, call kernel
-
-    mov AL, [num]       ; Get first byte at [num]
-    inc AL              ; Pick next letter
-
-    pop ecx             ; Store ECX value to stack
-    loop l1             ; Loop - decrement ECX and jump if ECX != 0
-
-end:
-    mov ebx, 0          ; Return value 0
-    mov eax, SYS_EXIT   ; sys_exit
-    int 0x80            ; system call by interruption
-
-; Data Section --------------------------------------------
-section .data
-num: db '1',0xA
+segment .data           ; data section
+msg:    db 'Hello World',0xA     ; the string to print, 10=cr
+len:    equ $ - msg     ; "$" means "here"
+                        ; len is a value, not an address
